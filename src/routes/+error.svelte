cf50b739991b88c380b8a34866c98aa75695abf6
justdimaa <justdimaa@duck.com>
2025-05-23 01:17:25 +0200

    fix: use page state instead of deprecated store

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <div class="flex w-screen flex-col items-center justify-center gap-2 p-4">
-	<h1 class="text-xl">{$page.status} | {$page.error?.message} 😭</h1>
+	<h1 class="text-xl">{page.status} | {page.error?.message} 😭</h1>
 	<a href="/" style="text-decoration: underline;">Go back to home page</a>
 </div>
